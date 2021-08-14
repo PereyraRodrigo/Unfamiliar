b6744d2c485205a6e248c0af465b27f5531448bc
Rodrigo Pereyra <pereyrarodrigo@hotmail.com>
2021-08-14 02:49:06 -0300

    added clock.js, footer, new navbar & stuff

---
+++ js/clock.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    var clock;
+
+    // Grab the current date
+    var currentDate = new Date();
+
+    // Target future date/24 hour time/Timezone
+    var targetDate = moment.tz("2021-09-30 23:59", "Australia/Sydney");
+
+    // Calculate the difference in seconds between the future and current date
+    var diff = targetDate / 1000 - currentDate.getTime() / 1000;
+
+    if (diff <= 0) {
+        // If remaining countdown is 0
+        clock = $(".clock").FlipClock(0, {
+            clockFace: "DailyCounter",
+            countdown: true,
+            autostart: false
+        });
+        console.log("Date has already passed!")
+
+    } else {
+        // Run countdown timer
+        clock = $(".clock").FlipClock(diff, {
+            clockFace: "DailyCounter",
+            countdown: true,
+            callbacks: {
+                stop: function () {
+                    console.log("Timer has ended!")
+                }
+            }
+        });
+
+        // Check when timer reaches 0, then stop at 0
+        setTimeout(function () {
+            checktime();
+        }, 1000);
+
+        function checktime() {
+            t = clock.getTime();
+            if (t <= 0) {
+                clock.setTime(0);
+            }
+            setTimeout(function () {
+                checktime();
+            }, 1000);
+        }
+    }
+});
